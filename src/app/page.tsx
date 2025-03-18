bf165558c38d2df49426f1be28683e7a03d6f471
Michel <djoumessi.michel08@gmail.com>
2025-03-18 09:07:42 +0100

    just change the name of the website in layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
+    <div className="flex flex-col items-center justify-center h-screen text-3xl">
       <h1>Hello world</h1>
       <p>Hey mister frozen :)</p>
     </div>
