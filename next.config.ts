38c55e81f1ff06b703e7a7cf297a25327c72b072
Michel <djoumessi.michel08@gmail.com>
2025-03-18 17:54:11 +0100

    create all folder and file, and start to code the login page

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config, { isServer }) => {
+    // Ajouter l'insensibilité à la casse
+    config.module.rules.push({
+      test: /\.js$/,
+      resolve: {
+        fullySpecified: false,
+      },
+    });
+
+    // Désactiver le contrôle de casse dans webpack
+    if (config.resolve) {
+      config.resolve.symlinks = false;
+    }
+
+    return config;
+  },
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-import Image from "next/image";
+import Login from "@/pages/auth/login";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen text-3xl">
-      <h1>Hello world</h1>
-      <p>Hey mister frozen :)</p>
+    <div>
+      <Login />
     </div>
   );
 }
