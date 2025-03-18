e1a1836fa46ffb15476e789d0499764b0e647b2e
Michel <djoumessi.michel08@gmail.com>
2025-03-18 08:52:10 +0100

    init commit :)

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1>Hello world</h1>
+      <p>Hey mister frozen :)</p>
+    </div>
+  );
+}
