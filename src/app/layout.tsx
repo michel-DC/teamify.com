b9a9806b13044ed75fd5fa533a325f775ff06afe
Michel <djoumessi.michel08@gmail.com>
2025-04-01 21:56:59 +0200

    fix all the outerLayout bug

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,7 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "../styles/globals.css";
 
-// Correctly import the font with required weight
-// const poppins = Poppins({ weight: "400", subsets: ["latin"], display: "swap" });
+const poppins = Poppins({ weight: "400", subsets: ["latin"], display: "swap" });
 
 export const metadata: Metadata = {
   title: "teamify.com",
@@ -18,7 +17,7 @@ export default function RootLayout({
   return (
     <html
       lang="fr"
-      // className={`${poppins.className} h-full bg-background text-foreground`}
+      className={`${poppins.className} h-full bg-background text-foreground`}
     >
       <body>{children}</body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
-import { Forgot } from "@/pages/auth/forgot";
-import { Login } from "@/pages/auth/login";
-import { Register } from "@/pages/auth/register";
+// import { Forgot } from "@/pages/auth/forgot";
+// import { Login } from "@/pages/auth/login";
+// import { Register } from "@/pages/auth/register";
 import { HomePage } from "@/pages/homePage";
 
 export default function Home() {
